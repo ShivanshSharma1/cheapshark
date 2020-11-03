9d72e579dbb4d84295d2deb34564aa91e2fab52e
ShivanshSharma1 <shivanshs331@gmail.com>
2020-11-03 23:44:51 +0530

    trying to deploy

--- src/App.js
+++ src/App.js
@@ -5,12 +5,12 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Headers = () => {
   return <thead class='thead-dark'>
-      <tr style={{}}>
-        <th scope='col' style={{width: '30%%', paddingLeft:"25px", fontFamily:'Syne Mono'}} class="h4">TITLE</th>
-        <th scope='col' style={{width: '20%', fontFamily:'Syne Mono'}} class="h4">Normal Price</th>
-        <th scope='col' style={{width: '15%', fontFamily:'Syne Mono'}} class="h4">Sale Price</th>
-      </tr>
-    </thead>
+    <tr style={{}}>
+      <th scope='col' style={{ width: '30%%', paddingLeft: "25px", fontFamily: 'Syne Mono' }} class="h4">TITLE</th>
+      <th scope='col' style={{ width: '20%', fontFamily: 'Syne Mono' }} class="h4">Normal Price</th>
+      <th scope='col' style={{ width: '15%', fontFamily: 'Syne Mono' }} class="h4">Sale Price</th>
+    </tr>
+  </thead>
 }
 
 const Game = ({ game }) => {
@@ -20,48 +20,53 @@ const Game = ({ game }) => {
   `
   return (
 
-    <tr style={{padding:'0px'}}>
-      <td style={{width:'37%', padding:'0px', paddingBottom:'0px'}}>
+    <tr style={{ padding: '0px' }}>
+      <td style={{ width: '37%', padding: '0px', paddingBottom: '0px' }}>
         <td
-        style={{float:'left',display:'inline-block', width:'120px', height:'45px', textAlign:'center', padding:'0px 0px 0px 0px', border:'0px solid', backgroundColor:'black', }}>
-          <Img alt="404" src={game.thumb} class="img-fluid" style={{padding:'0px'}}/>
+          style={{ float: 'left', display: 'inline-block', width: '120px', height: '45px', textAlign: 'center', padding: '0px 0px 0px 0px', border: '0px solid', backgroundColor: 'black', }}>
+          <Img alt="404" src={game.thumb} class="img-fluid" style={{ padding: '0px' }} />
         </td>
-        <div style={{marginTop:'10px'}}>
-        <a
-          style={{float:'left'}}
-          href={`https://www.cheapshark.com/redirect?dealID=${game.dealID}`}>
-          {game.title}
-        </a>
+        <div style={{ marginTop: '10px' }}>
+          <a
+            style={{ float: 'left' }}
+            href={`https://www.cheapshark.com/redirect?dealID=${game.dealID}`}>
+            {game.title}
+          </a>
         </div>
       </td>
-      <td style={{padding:'10px 0px 0px 0px',color:'white'}}>Normal Price -{game.normalPrice}</td>
-      <td style={{padding:'10px 0px 0px 0px', color:'white'}}>Sale Price-{game.salePrice}</td>
+      <td style={{ padding: '10px 0px 0px 0px', color: 'white' }}>Normal Price -{game.normalPrice}</td>
+      <td style={{ padding: '10px 0px 0px 0px', color: 'white' }}>Sale Price-{game.salePrice}</td>
     </tr>
   )
 }
 
 const AllGames = ({ games }) => {
 
-  return <Fragment><div style={{width:'80%', backgroundColor:'black', position:'relative', left:'9%', border:'25px solid grey', borderRadius:'25px'}}><table class="table table-border" style={{width:'100%'}}><Headers /><tbody>{games ? games.map(g => <Game game={g} />) : <div></div>}</tbody></table></div></Fragment>
+  return <Fragment><div style={{ width: '80%', backgroundColor: 'black', position: 'relative', left: '9%', border: '25px solid grey', borderRadius: '25px' }}><table class="table table-border" style={{ width: '100%' }}><Headers /><tbody>{games ? games.map(g => <Game game={g} />) : <div></div>}</tbody></table></div></Fragment>
 }
 
 const App = () => {
   let [games, setGames] = useState();
-  useEffect(async () => {
-    const response = await fetch('https://www.cheapshark.com/api/1.0/deals?');
-    const data = await response.json();
-    await setGames(data)
-    await console.log(data)}, [])
-  const Background = 'https://i.pinimg.com/originals/f5/67/99/f56799d20e790b1df5bbef8f537579e5.jpg'
-  return (
-    <div style={{
-      background:`url(${Background})`,
-      backgroundColor:'white'
-      }}>
-      <Nav/>
-      <AllGames games={games} />
-    </div>
-  )
+
+  useEffect(() => { 
+    async function fetchMyAPI (){
+      const response = await fetch('https://www.cheapshark.com/api/1.0/deals?');
+      const data = await response.json();
+      setGames(data)
+    }
+      fetchMyAPI();
+    }, [])
+
+const Background = 'https://i.pinimg.com/originals/f5/67/99/f56799d20e790b1df5bbef8f537579e5.jpg'
+return (
+  <div style={{
+    background: `url(${Background})`,
+    backgroundColor: 'white'
+  }}>
+    <Nav />
+    <AllGames games={games} />
+  </div>
+)
 }
 
 export default App;
