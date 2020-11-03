f0e70b5073cc6b727d0a28ead2339ca04556d0b0
ShivanshSharma1 <shivanshs331@gmail.com>
2020-11-03 22:56:45 +0530

    some css changes

--- src/nav.js
+++ src/nav.js
@@ -3,7 +3,7 @@ import'bootstrap/dist/css/bootstrap.min.css';
 const Nav = () => {
     return (
         <div>
-        <nav class="navbar navbar-light bg-light">
+        <nav className="navbar navbar-light" >
         <div style={{width:'100%', textAlign:'center'}}>
             <img src="https://www.azfonts.net/preview.php?type=sample&id_font=139708&t=GAMEDEALS"></img>
         </div>
